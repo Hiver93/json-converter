14a326d63077ab73d6d3fa33600dff9bdb87031e
kdw3238 <kdw3238@naver.com>
2024-03-07 22:06:46 +0900

    [feat] PrimitiveWrapperMapping 이넘 클래스 작성
    primitive와 wrapper클래스를 매핑하여 사용하기 편하게 하기 위해 작성

---
+++ json-converter/src/json_converter/enums/PrimitiveWrapperMapping.java
@@ -0,0 +1,48 @@
+package json_converter.enums;
+
+import javax.management.RuntimeErrorException;
+
+public enum PrimitiveWrapperMapping {
+	BOOLEAN(boolean.class, Boolean.class),
+    BYTE(byte.class, Byte.class),
+    SHORT(short.class, Short.class),
+    INTEGER(int.class, Integer.class),
+    LONG(long.class, Long.class),
+    FLOAT(float.class, Float.class),
+    DOUBLE(double.class, Double.class),
+    CHARACTER(char.class, Character.class);
+
+    private final Class<?> primitive;
+    private final Class<?> wrapper;
+
+    PrimitiveWrapperMapping(Class<?> primitive, Class<?> wrapper) {
+        this.primitive = primitive;
+        this.wrapper = wrapper;
+    }
+    
+    public Class<?> getPrimitiveType() {
+        return primitive;
+    }
+
+    public Class<?> getWrapperType() {
+        return wrapper;
+    }
+
+    public static Class<?> getWrapperByPrimitive(Class<?> primitiveClass) {
+        for (PrimitiveWrapperMapping mapping : values()) {
+            if (mapping.primitive == primitiveClass) {
+                return mapping.wrapper;
+            }
+        }
+        throw new RuntimeException("Invalid primitive type");
+    }
+
+    public static Class<?> getPrimitiveByWrapper(Class<?> wrapperClass) {
+        for (PrimitiveWrapperMapping mapping : values()) {
+            if (mapping.wrapper == wrapperClass) {
+                return mapping.primitive;
+            }
+        }
+        throw new RuntimeException("Invalid wrapper class");
+    }
+}
