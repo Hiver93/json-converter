53bb8e751576d9422c97310020f5c941e1ad9565
kdw3238 <kdw3238@naver.com>
2024-03-16 22:05:31 +0900

    [refactor] JsonParser 코드 수정 (TypeContainer)
    JsonParser가 TypeContainer를 사용하여 파실을 실행하도록 변경

--- json-converter/src/json_converter/parser/JsonParser.java
+++ json-converter/src/json_converter/parser/JsonParser.java
@@ -1,62 +1,54 @@
 package json_converter.parser;
 
-import java.util.List;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
+import java.lang.reflect.TypeVariable;
 import java.util.Collection;
-import java.util.Deque;
+import java.util.List;
 import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
 
 import json_converter.enums.EscapeSequence;
-import json_converter.enums.Parentheses;
 import json_converter.enums.PrimitiveWrapperMapping;
-import json_converter.generic.container.GenericTypeContainer;
-import json_converter.generic.type.TypeToken;
 import json_converter.instance.InstanceFactory;
 import json_converter.tokenizer.JsonTokenizer;
 import json_converter.tokenizer.factory.JsonTokenizerFactory;
+import json_converter.type.TypeContainer;
 
 public class JsonParser {	
-	public <T> T parse(String jsonStr, Class<T> cl) {
+	
+	public <T> T parse(String jsonStr, TypeContainer tc) {
 		if(JsonTokenizer.isNullValue(jsonStr)) {
 			return null;
 		}
 		T object = null;
 		try {
-			if(GenericTypeContainer.isGenericContainer(cl)) {
-				
+			if(Map.class.isAssignableFrom(tc.getBaseType())) {
+				object = mapToMap(jsonStr,tc);
 			}
-			if(Map.class.isAssignableFrom(cl)) {
-				object = mapToMap(jsonStr,cl);
+			else if(Collection.class.isAssignableFrom(tc.getBaseType())) {
+				object = mapToCollection(jsonStr, tc);
 			}
-			else if(Collection.class.isAssignableFrom(cl)) {
-				object = mapToCollection(jsonStr, cl);
+			else if(tc.getBaseType().isArray()) {
+				object = mapToArray(jsonStr, tc);
 			}
-			else if(cl.isArray()) {
-				object = mapToArray(jsonStr, cl);
-			}
-			else if(String.class.equals(cl)) {
+			else if(String.class.equals(tc.getBaseType())) {
 				object = (T)mapToString(jsonStr);
 			}
-			else if(Number.class.isAssignableFrom(cl)) {
-				object = (T)mapToNumber(jsonStr, cl);
+			else if(Number.class.isAssignableFrom(tc.getBaseType())) {
+				object = (T)mapToNumber(jsonStr, tc);
 			}
-			else if(Character.class.equals(cl)) {
+			else if(Character.class.equals(tc.getBaseType())) {
 				object = (T)mapToChar(jsonStr);
 			}
-			else if(Boolean.class.equals(cl)) {
+			else if(Boolean.class.equals(tc.getBaseType())) {
 				object = (T)mapToBool(jsonStr);
 			}
-			else if(cl.isPrimitive()) {
-				object = mapToPrimitive(jsonStr,cl);
+			else if(tc.getBaseType().isPrimitive()) {
+				object = mapToPrimitive(jsonStr,tc);
 			}
-			else if(!cl.getName().startsWith("java")){
-				object = mapToObject(jsonStr,cl);
+			else if(!tc.getBaseType().getName().startsWith("java")){
+				object = mapToObject(jsonStr,tc);
 			}
 			else {
 				throw new RuntimeException("Unsupported type format");
@@ -66,37 +58,83 @@ public class JsonParser {
 		}
 		return object;
 	}
-
-	public <T>T parse(String jsonStr, Type type){
-//		T object = null;
-		
-		return parse(jsonStr,(Class<T>)type);
-	}
 	
-	public <T>T parse(String jsonStr, TypeToken<T> typeToken){
-		if(GenericTypeContainer.isGenericContainer(typeToken.getType())) {
-			GenericTypeContainer gc = new GenericTypeContainer(typeToken);
+	private <T>T mapToObject(String jsonStr, TypeContainer tc) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
+		T t = (T)InstanceFactory.newInstance(tc.getBaseType());
+		while(tokenizer.hasMoreTokens()) {
+			Field field = tc.getBaseType().getDeclaredField(parse(tokenizer.next(),new TypeContainer(String.class)));
+			field.setAccessible(true);
+			field.set(t, parse(tokenizer.next(), new TypeContainer(field.getType())));
 		}
-		return (T) parse(jsonStr,typeToken.getType());
+		return t;
 	}
-	
+
+	private <T>T mapToArray(String jsonStr, TypeContainer tc) {
+		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
+		List list = InstanceFactory.newInstance(List.class);
+		TypeContainer componentTc = new TypeContainer(tc.getBaseType().getComponentType());
+		while(tokenizer.hasMoreTokens()) {
+			list.add(parse(tokenizer.next(),componentTc));
+		}
+		T arr = (T)Array.newInstance(componentTc.getBaseType(), list.size());
+		for(int i = 0; i < list.size(); ++i) {
+			Array.set(arr, i, list.get(i));
+		}
+		return arr;
+	}
+
+	private <T>T mapToPrimitive(String jsonStr, TypeContainer tc) {
+		return (T) parse(jsonStr, new TypeContainer(PrimitiveWrapperMapping.getWrapperByPrimitive(tc.getBaseType())));
+	}
+
+	private <T>T mapToNumber(String jsonStr, TypeContainer tc) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		Number num = (Number)tc.getBaseType().getDeclaredMethod("valueOf", String.class).invoke(null, jsonStr);
+		return (T)num;
+	}
+
+	private <T>T mapToCollection(String jsonStr, TypeContainer tc) {
+		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
+		T t= (T)InstanceFactory.newInstance(tc.getBaseType());
+		if(tc.getGenericTypes()[0] instanceof TypeVariable) {
+			while(tokenizer.hasMoreTokens()) {
+				((Collection)t).add(parse(tokenizer.next()));
+			}
+		}
+		else {
+			while(tokenizer.hasMoreTokens()) {
+				((Collection)t).add(parse(tokenizer.next(), new TypeContainer(tc.getGenericTypes()[0])));
+			}
+		}
+		return t;
+	}
+
+	private <T>T mapToMap(String jsonStr, TypeContainer tc) {
+		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
+		T t = (T) InstanceFactory.newInstance(tc.getBaseType());
+		while(tokenizer.hasMoreTokens()) {
+			Object key  = null;
+			Object value = null;
+			if(tc.getGenericTypes()[0] instanceof TypeVariable) {
+				key = parse(tokenizer.next());
+				value = parse(tokenizer.next());
+			}
+			else {
+				key = parse(tokenizer.next(), new TypeContainer(tc.getGenericTypes()[0]));
+				value = parse(tokenizer.next(), new TypeContainer(tc.getGenericTypes()[1]));
+			}
+			((Map)t).put(key, value);
+		}
+		return t;
+	}
 	public <T>T parse(String jsonStr){
 		T t = null;
 		Class<?> cl = JsonTokenizer.inferClass(jsonStr);
 		if(cl == null) {
 			return null;
 		}		
-		t = (T)parse(jsonStr,cl);
+		t = (T)parse(jsonStr,new TypeContainer(cl));
 		return t;
-	}
-	
-	private <T>T mapToPrimitive(String jsonStr, Class<T> cl) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		return (T) parse(jsonStr, PrimitiveWrapperMapping.getWrapperByPrimitive(cl));
-	}
-
-	private Number mapToNumber(String jsonStr, Class<?> cl) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
-		Number num = (Number)cl.getDeclaredMethod("valueOf", String.class).invoke(null, jsonStr);
-		return num;
 	}
 
 	private String mapToString(String jsonStr) {
@@ -131,49 +169,5 @@ public class JsonParser {
 	
 	private Boolean mapToBool(String jsonStr) {
 		return Boolean.valueOf(jsonStr);
-	}
-	private <T>T mapToObject(String jsonStr, Class<T> cl) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
-		T t = InstanceFactory.newInstance(cl);
-		while(tokenizer.hasMoreTokens()) {
-			Field field = cl.getDeclaredField(parse(tokenizer.next(),String.class));
-			field.setAccessible(true);
-			field.set(t, parse(tokenizer.next(),field.getType()));
-		}
-		return t;
-	}
-	
-	private <T>T mapToCollection(String jsonStr, Class<T> cl){
-		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
-		T t= InstanceFactory.newInstance(cl);
-		while(tokenizer.hasMoreTokens()) {
-			((Collection)t).add(parse(tokenizer.next()));
-		}
-		return t;
-	}
-	
-	private <T>T mapToMap(String jsonStr, Class<T> cl) {
-		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
-		T t = InstanceFactory.newInstance(cl);
-		while(tokenizer.hasMoreTokens()) {
-			Object key = parse(tokenizer.next());
-			Object value = parse(tokenizer.next());
-			((Map)t).put(key, value);
-		}
-		return t;
-	}
-
-	private <T>T mapToArray(String jsonStr, Class<T> cl) {
-		JsonTokenizer tokenizer = JsonTokenizerFactory.jsonTokenizer(jsonStr);
-		List list = InstanceFactory.newInstance(List.class);
-		Class<?> componentType = cl.getComponentType();
-		while(tokenizer.hasMoreTokens()) {
-			list.add(parse(tokenizer.next(),componentType));
-		}
-		T arr = (T)Array.newInstance(componentType, list.size());
-		for(int i = 0; i < list.size(); ++i) {
-			Array.set(arr, i, list.get(i));
-		}
-		return arr;
 	}
 }
