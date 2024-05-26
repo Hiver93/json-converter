02a44c7705046404a456466dc805f0647a76f514
kdw3238 <kdw3238@naver.com>
2024-05-26 23:22:58 +0900

    [test] TypeContainerTest getFieldTypeContainer()
    getFieldTypeContainer 테스트 작성

--- json-converter/src/json_converter/test/TypeContainerTest.java
+++ json-converter/src/json_converter/test/TypeContainerTest.java
@@ -50,4 +50,25 @@ public class TypeContainerTest {
 		}
 	}
 	
+	@Test
+	public void getFieldTypeContainer() throws NoSuchFieldException, SecurityException {
+		TypeContainer typeContainer = new TypeContainer(new TypeToken<MyClass<Integer>>() {});
+		List<Type> types = List.of(
+				typeContainer.getFieldTypeContainer(typeContainer.getBaseClass().getDeclaredField("list")).getBaseClass(),
+				typeContainer.getFieldTypeContainer(typeContainer.getBaseClass().getDeclaredField("i")).getBaseClass(),
+				typeContainer.getFieldTypeContainer(typeContainer.getBaseClass().getDeclaredField("tList")).getBaseClass(),
+				typeContainer.getFieldTypeContainer(typeContainer.getBaseClass().getDeclaredField("t")).getBaseClass()
+				);
+		List<Type> typeExpecteds = List.of(
+				List.class,
+				int.class,
+				List.class,
+				Integer.class
+				);
+		for(int i = 0; i < types.size(); ++i) {
+			TypeContainer tc = new TypeContainer(types.get(i));
+			assertEquals(typeExpecteds.get(i), types, "not equals: idx " + i);
+		}
+	}
+	
 }	
